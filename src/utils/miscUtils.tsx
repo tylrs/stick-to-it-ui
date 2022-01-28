2112161821ea1b1ac08b53ee8831731753287f6e
Taylor Galloway <galloway.taylor@gmail.com>
2022-01-28 17:36:07 -0600

    Add store and get user utils

--- src/utils/miscUtils.tsx
+++ src/utils/miscUtils.tsx
@@ -5,3 +5,11 @@ export const storeToken = (token: string) => {
 export const getToken = () => {
     return JSON.stringify(localStorage.getItem("token"))
 }
+
+export const storeCurrentUser = (user: any) => {
+    localStorage.setItem("currentUser", JSON.stringify(user))
+}
+
+export const getCurrentUser = () => {
+    return JSON.stringify(localStorage.getItem("currentUser"))
+}
