b1ab9d1ca64b74f5d18e67d1645a6a70332a8fd5
Taylor Galloway <galloway.taylor@gmail.com>
2022-01-28 17:35:18 -0600

    Add url object for different environments

---
+++ src/dev-constants.tsx
@@ -0,0 +1,6 @@
+export const urls = {
+    productionUsers: "https://stick-to-it-api.herokuapp.com/users",
+    localUsers: "http://localhost:3000/users",
+    productionLogin: "https://stick-to-it-api.herokuapp.com/auth/login",
+    localLogin: "http://localhost:3000/auth/login" 
+}
