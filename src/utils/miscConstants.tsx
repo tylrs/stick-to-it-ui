020ffd6c8497a8f86000135cdc2cc70e3d959e77
Taylor Galloway <galloway.taylor@gmail.com>
2022-01-31 09:29:30 -0600

    Add blank state constants

---
+++ src/utils/miscConstants.tsx
@@ -0,0 +1,14 @@
+export const emptyUser = {
+    id: 0,
+    name: "",
+    username: "",
+    email: ""
+  }
+
+export const blankAccount = {
+    name: "", 
+    username: "", 
+    email: "", 
+    password: "", 
+    passwordConfirmation: ""
+}
