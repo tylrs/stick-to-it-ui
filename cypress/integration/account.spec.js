747fbf6ebdf18ce26c3842619c6c31051c9f63f2
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-02 12:34:59 -0600

    Add test for creating account

--- cypress/integration/account.spec.js
+++ cypress/integration/account.spec.js
@@ -1,6 +1,42 @@
-// account.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+describe("Account User Flow", () => {
+    beforeEach(() => {
+        cy.intercept("POST", "https://stick-to-it-api.herokuapp.com/users", {
+            ok: true,
+            statusCode: 201, 
+            fixture: "newAccountResponse"
+        }).as("Create Account")
+
+        cy.intercept("POST", "https://stick-to-it-api.herokuapp.com/auth/login", {
+            ok: true,
+            statusCode: 200, 
+            fixture: "loginResponse1"
+        }).as("Log In")
+
+        cy.visit("http://localhost:2000/")
+    })
+
+    it("Should be able to create an account ", () => {
+        cy
+            .get('[href="/create-account"] > .account-link-wrapper')
+            .click()
+            .get(".account-page-header")
+            .should("be.visible")
+            .get('[name="name"]')
+            .type("Sample Bob")
+            .get('[name="username"]')
+            .type("samplebob1")
+            .get('[name="email"]')
+            .type("samplebob1@sample.com")
+            .get('[name="password"]')
+            .type("123456")
+            .get('[name="passwordConfirmation"]')
+            .type("123456")
+            .get(".submit-account-creation")
+            .click()
+            .wait("@Log In")  
+            .url()
+            .should("include", "/all-habits")
+            .get(".greeting-message")
+            .should("have.text", "Welcome: Sample Bob")       
+    })
+})
