738e2239d6fbce87b92fef9b525f8492783ab435
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-02 12:07:48 -0600

    Add list of intercepts

---
+++ cypress/support/interceptsList.js
@@ -0,0 +1,40 @@
+//Create Account
+cy.intercept("POST", "https://stick-to-it-api.herokuapp.com/users", {
+    ok: true,
+    statusCode: 201, 
+    fixture: "newAccountResponse"
+}).as("Create Account")
+
+//Log In
+cy.intercept("POST", "https://stick-to-it-api.herokuapp.com/auth/login", {
+    ok: true,
+    statusCode: 200, 
+    fixture: "loginResponse1"
+}).as("Log In")
+
+//Create New Habit
+cy.intercept("POST", "https://stick-to-it-api.herokuapp.com/users/:id/habits", {
+    ok: true,
+    statusCode: 201, 
+    fixture: "newHabitResponse"
+}).as("Create New Habit")
+
+//Get Habits From One User
+cy.intercept("GET", "https://stick-to-it-api.herokuapp.com/users/:id/habits", {
+    ok: true,
+    statusCode: 200, 
+    fixture: "allHabitsResponse"
+}).as("Get User Habits")
+
+//Get Habits After Adding One Habit
+cy.intercept("GET", "https://stick-to-it-api.herokuapp.com/users/:id/habits", {
+    ok: true,
+    statusCode: 200, 
+    fixture: "newAllHabitsResponse"
+}).as("Get User Habits After Update")
+
+//Delete Habit
+cy.intercept("DELETE", "https://stick-to-it-api.herokuapp.com/users", {
+    ok: true,
+    statusCode: 204
+}).as("Create Account")
