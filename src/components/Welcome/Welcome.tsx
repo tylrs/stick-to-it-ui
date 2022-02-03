e37228ecf7770b5b6ac317fc85b160c59a65b253
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-03 13:57:59 -0600

    Add another class name to welcome page buttons

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -5,10 +5,10 @@ import "./Welcome.css";
 const Welcome = () => {
     return (
         <section className="welcome-page-container">
-            <Link className="account-link" to="/login">
+            <Link className="account-link to-log-in" to="/login">
                 <button className="account-link-wrapper">Login</button>
             </Link>
-            <Link className="account-link" to="/create-account">
+            <Link className="account-link to-create-account" to="/create-account">
                 <button className="account-link-wrapper">Create Account</button>
             </Link>
         </section>
