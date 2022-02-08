ac3b59b44ab083ee24d72976969b0d3108f25581
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-07 20:45:41 -0600

    Add HabitLog controlled form

--- src/components/HabitLog/HabitLog.tsx
+++ src/components/HabitLog/HabitLog.tsx
@@ -1,11 +1,27 @@
 import React, { useEffect, useState } from "react";
 import "./HabitLog.css";
 import { HabitLogType } from "../../utils/types";
+import { daysOfWeek } from "../../utils/miscConstants";
 
-const HabitLog: React.FC<{habitLogInfo: HabitLogType}> = ({ habitLogInfo }) => {
+const HabitLog: React.FC<{habitLogInfo: HabitLogType | null, dayNum: number}> = ({ habitLogInfo, dayNum }) => {
+    const [completed, setCompleted] = useState(false)
+    
+    const dayOfWeek = daysOfWeek[dayNum]
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.target.checked ? setCompleted(true) : setCompleted(false)
+    }
+
+    useEffect(() => {
+        if (habitLogInfo?.completed_at) {
+            setCompleted(true)
+        }
+    }, [])
+
     return (
         <div className="habit-log-container">
-            <h4>{habitLogInfo.scheduled_at}</h4>
+            <h4>{dayOfWeek}</h4>
+            <input type="checkbox"checked={completed} onChange={(e) => handleChange(e)}/>
         </div>
     )
 }
