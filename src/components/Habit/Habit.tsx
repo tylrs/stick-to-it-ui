d9a4d62e7812fe3639366b6a8e4cabc560194f0d
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-07 18:52:12 -0600

    Render basic logs from Habit Component

--- src/components/Habit/Habit.tsx
+++ src/components/Habit/Habit.tsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Habit.css";
-import { HabitType } from "../../utils/types";
+import { HabitLogType, HabitType } from "../../utils/types";
+import HabitLog from "../HabitLog/HabitLog";
 
-const Habit: React.FC<{habit: HabitType, handleDelete: any}> = ({ habit, handleDelete }) => {
+const Habit: React.FC<{habitInfo: HabitType, habitLogs: HabitLogType[], handleDelete: any}> = ({ habitInfo, habitLogs, handleDelete }) => {
+    const allLogs = habitLogs.map((habitLogInfo) => <HabitLog habitLogInfo={habitLogInfo} key={habitLogInfo.id}/>)
 
     return (
         <article className="habit-container">
-            <h3 className="habit-name">Habit: {habit.name}</h3>
-            <p>Description: {habit.description}</p>
-            <button className="habit-delete-button"onClick={() => {handleDelete(habit.id)}}>Delete</button>
+            <h3 className="habit-name">Habit: {habitInfo.name}</h3>
+            <p>Description: {habitInfo.description}</p>
+            {allLogs}
+            <button className="habit-delete-button"onClick={() => {handleDelete(habitInfo.id)}}>Delete</button>
         </article>
     )
 }
