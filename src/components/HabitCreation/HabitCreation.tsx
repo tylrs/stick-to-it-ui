75346be898542aec3190297811efdfad2bcca465
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-11 11:05:56 -0600

    Change util name

--- src/components/HabitCreation/HabitCreation.tsx
+++ src/components/HabitCreation/HabitCreation.tsx
@@ -6,7 +6,7 @@ import { blankHabit } from "../../utils/miscConstants";
 import { HabitType } from "../../utils/types";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { checkHabitCreation } from "../../utils/miscUtils";
+import { checkFormSubmission } from "../../utils/miscUtils";
 
 const HabitCreation: React.FC<{userId: number, setMessage: React.Dispatch<React.SetStateAction<string>>}> = ({ userId, setMessage }) => {
     const [habitInfo, setHabitInfo] = useState<HabitType>(blankHabit)
@@ -62,7 +62,7 @@ const HabitCreation: React.FC<{userId: number, setMessage: React.Dispatch<React.
         const habitData = habitInfo
         habitData.userId = userId
         try {
-            checkHabitCreation(habitData)
+            checkFormSubmission(habitData)
             await createHabit(habitData)
             navigate("/all-habits")
             setMessage("New Habit Created")
