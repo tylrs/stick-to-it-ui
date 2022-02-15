dfc7cc1b3ed9611b3dc4157b92f5e89cd1440665
Taylor Galloway <galloway.taylor@gmail.com>
2022-02-15 11:28:49 -0600

    Remove link import

--- src/components/AccountCreation/AccountCreation.tsx
+++ src/components/AccountCreation/AccountCreation.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { createUser, login } from "../../utils/apiCalls";
 import { blankAccount } from "../../utils/miscConstants";
 import { AccountType, UserType } from "../../utils/types";
