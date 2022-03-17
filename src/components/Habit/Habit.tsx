c3db5d66faf6815b846a0eb9d5f8a8870a7fd33a
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-17 16:14:23 -0500

    Render habit plans

--- src/components/Habit/Habit.tsx
+++ src/components/Habit/Habit.tsx
@@ -2,31 +2,41 @@ import "./Habit.css";
 import { HabitProps } from "../../utils/types";
 import HabitLog from "../HabitLog/HabitLog";
 import { generateHabitLogList, getDayOfWeek } from "../../utils/miscUtils";
+import HabitPlan from "../HabitPlan/HabitPlan";
 
 const Habit: React.FC<HabitProps> = ({
   habitInfo,
-  habitLogsInfo,
+  habitPlans,
   handleDelete,
   listType,
   setMessage,
 }) => {
   let allLogs;
 
-  if (listType === "all") {
-    allLogs = generateHabitLogList({
-      habitInfo,
-      habitLogsInfo,
-      listType,
-      setMessage,
-    });
-  }
+  // if (listType === "all") {
+  //   allLogs = generateHabitLogList({
+  //     habitPlanInfo,
+  //     habitLogsInfo,
+  //     listType,
+  //     setMessage,
+  //   });
+  // }
+  const formattedHabitPlans = habitPlans.map(habitPlan => (
+    <HabitPlan
+      habitPlanInfo={habitPlan}
+      habitLogsInfo={habitPlan.habit_logs}
+      handleDelete={handleDelete}
+      listType={listType}
+      setMessage={setMessage}
+    />
+  ));
 
   return (
     <article className={`habit-container-${listType}`}>
       <div className="habit-info-container">
-        <h3 className="habit-name">{habitInfo.habit.name}</h3>
-        <p className="habit-description">{habitInfo.habit.description}</p>
-        {listType !== "all" && (
+        <h3 className="habit-name">{habitInfo.name}</h3>
+        <p className="habit-description">{habitInfo.description}</p>
+        {/* {listType !== "all" && (
           <HabitLog
             habitLogInfo={habitLogsInfo[0]}
             userId={habitInfo.userId}
@@ -41,9 +51,9 @@ const Habit: React.FC<HabitProps> = ({
             handleDelete(habitInfo.habit_id);
           }}>
           Delete
-        </button>
+        </button> */}
       </div>
-      {listType === "all" && (
+      {/* {listType === "all" && (
         <div className="habit-logs-container">{allLogs}</div>
       )}
       <button
@@ -52,7 +62,8 @@ const Habit: React.FC<HabitProps> = ({
           handleDelete(habitInfo.habit_id);
         }}>
         Delete
-      </button>
+      </button> */}
+      {formattedHabitPlans}
     </article>
   );
 };
