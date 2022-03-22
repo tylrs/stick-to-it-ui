198d1367b29a69ec5131fe2f3e15f280502c2704
Taylor Galloway <galloway.taylor@gmail.com>
2022-03-22 14:04:01 -0500

    Add nav link styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Message from "../Message/Message";
 import "./Header.css";
 
@@ -21,12 +21,12 @@ const Header: React.FC<HeaderProps> = ({
       {headerType === "loggedIn" && (
         <div className="link-container">
           <Message message={message} />
-          <Link className="header-link" to="/today">
+          <NavLink className="header-link" to="/today">
             Habits Today
-          </Link>
-          <Link className="header-link" to="/all-habits">
+          </NavLink>
+          <NavLink className="header-link" to="/all-habits">
             Habits Week
-          </Link>
+          </NavLink>
           <button className="log-out-button" onClick={() => logOut()}>
             Log Out
           </button>
