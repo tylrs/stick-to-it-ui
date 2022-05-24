7bcaa514bf9073041c9d113e025cb4963fd913d8
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-24 12:05:35 -0500

    Add first form step

--- src/components/InviteModal/InviteModal.tsx
+++ src/components/InviteModal/InviteModal.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { formatDateTime } from "../../utils/miscUtils";
 import { InviteModalProps } from "../../utils/types";
 
@@ -5,18 +6,32 @@ const InviteModal: React.FC<InviteModalProps> = ({
   habitPlanInfo,
   showInviteModal,
 }) => {
+  const [formStep, setFormStep] = useState(1);
+
   if (!showInviteModal) return null;
-  return (
-    <div>
+  const invitationHeader = (
+    <>
       <h2>Invitation for:</h2>
       <h3>{habitPlanInfo.habit.name}</h3>
       <p className="habit-plan-invite-date-range">
-        {` ${formatDateTime(habitPlanInfo.start_datetime)}-${formatDateTime(
+        {`${formatDateTime(habitPlanInfo.start_datetime)}-${formatDateTime(
           habitPlanInfo.end_datetime
         )}`}
       </p>
-    </div>
+    </>
   );
+  switch (formStep) {
+    case 1:
+      return (
+        <div>
+          {invitationHeader}
+          <button>Find user by email</button>
+          <button>Invite someone without account</button>
+        </div>
+      );
+    default:
+      return <></>;
+  }
 };
 
 export default InviteModal;
