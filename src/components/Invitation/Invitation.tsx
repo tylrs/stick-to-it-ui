0a301373dd6c9df1bd4dc36f0a91d83aca400764
Taylor Galloway <galloway.taylor@gmail.com>
2022-05-31 11:31:38 -0500

    Add styling for invitation card

--- src/components/Invitation/Invitation.tsx
+++ src/components/Invitation/Invitation.tsx
@@ -1,11 +1,12 @@
 import { formatDateTime } from "../../utils/miscUtils";
 import { InvitationProps } from "../../utils/types";
+import "./Invitation.css";
 
 const Invitation: React.FC<InvitationProps> = ({ type, invitationInfo }) => {
   const { habit_plan, sender } = invitationInfo;
   console.log(type, invitationInfo);
   return (
-    <article className="invitation">
+    <article className="invitation-card">
       <h3>{habit_plan.habit.name}</h3>
       <p className="invitation-habit-plan-date-range">
         {`${formatDateTime(habit_plan.start_datetime)}-${formatDateTime(
